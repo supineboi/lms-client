0836df5cc396699f48ed377ea12483550fb2c606
openRahil@786 <rahil.khan@opensenselabs.com>
2024-01-21 22:32:55 +0530

    correct url

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api/v1";
-const BASE_URL = process.env.BASE_URL;
-
+const BASE_URL = "https://mern-lms-app-back.onrender.com/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
